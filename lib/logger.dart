64fbfd885ed818d0a85b890c78cb28383a272a59
meanllbrl <meanllbrl@gmail.com>
2021-11-16 18:15:32 +0300

    logger customize edildi

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,6 +1,17 @@
 import 'package:logger/logger.dart' as lg;
 
-final _logger = lg.Logger();
+final _logger = lg.Logger(
+  printer:lg.PrettyPrinter(
+    methodCount: 2, // number of method calls to be displayed
+    errorMethodCount: 8, // number of method calls if stacktrace is provided
+    lineLength: 120, // width of the output
+    colors: true, // Colorful log messages
+    printEmojis: true, // Print an emoji for each log message
+    printTime: false ,
+    
+    
+  ) 
+);
 
 class Logger {
   static void success(String message) {
@@ -8,7 +19,7 @@ class Logger {
   }
 
   static void error(String message) {
-    _logger.e(message);
+    _logger.e(message,);
   }
 
   static void warning(String message) {
